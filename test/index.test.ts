7d907a700f3442ce24982f5869d7392c813b390c
Ryo Ota <nwtgck@gmail.com>
2019-09-29 11:25:36 +0900

    [add/test] Add readableStreamToUint8Array() and test it

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'power-assert';
 
 import {mergeUint8Arrays} from '../src/mergeUint8Arrays';
+import {readableStreamToUint8Array} from '../src/readableStreamToUint8Array';
 import {blobToReadableStream} from '../src/blobToReadableStream';
 import {blobToArrayBuffer} from '../src/blobToArrayBuffer';
 import {blobToUint8Array} from '../src/blobToUint8Array';
@@ -29,18 +30,29 @@ describe('mergeUint8Arrays', () => {
   });
 });
 
+describe('readableStreamToUint8Array', () => {
+  it('should convert ReadableStream to Uint8Array', async () => {
+    const readableStream: ReadableStream<Uint8Array> = new ReadableStream({
+      start(controller){
+        controller.enqueue(new Uint8Array([1, 2, 3]));
+        controller.enqueue(new Uint8Array([4, 5, 6, 7]));
+        controller.enqueue(new Uint8Array([8, 9]));
+        controller.close();
+      }
+    });
+    const actual: Uint8Array = await readableStreamToUint8Array(readableStream);
+    const expect = new Uint8Array([
+      1, 2, 3, 4, 5, 6, 7, 8, 9
+    ]);
+    assert.deepStrictEqual(actual, expect);
+  });
+});
+
 describe('blobToReadableStream', () => {
   it('should convert blob to ReadableStream', async () => {
     const blob = new Blob(["Lorem ipsum dolor sit amet, altera quidam in pro."]);
     const readableStream: ReadableStream<Uint8Array> = blobToReadableStream(blob);
-    const reader = readableStream.getReader();
-    const arrays: Uint8Array[] = [];
-    while (true) {
-      const {value, done} = await reader.read();
-      if (done) break;
-      arrays.push(value);
-    }
-    const array: Uint8Array = mergeUint8Arrays(arrays);
+    const array: Uint8Array = await readableStreamToUint8Array(readableStream);
     const expect: Uint8Array = new Uint8Array([
       76,111,114,101,109,32,105,112,115,117,109,32,100,111,108,111,114,32,115,105,116,32,97,109,101,116,44,32,97,108,116,101,114,97,32,113,117,105,100,97,109,32,105,110,32,112,114,111,46
     ]);
