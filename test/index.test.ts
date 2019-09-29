cacd39a9ed5fa59f7252453bdf05816169d5bfb4
Ryo Ota <nwtgck@gmail.com>
2019-09-29 10:07:11 +0900

    [add/test] Add mergeUint8Arrays() and add tests for it

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,25 @@
-// TODO: Write test
 import * as assert from 'power-assert';
 
-describe('1 should be 1', () => {
-  it('should pass', async () => {
-    assert.strictEqual(1, 1);
+import {mergeUint8Arrays} from '../src/mergeUint8Arrays';
+
+describe('mergeUint8Arrays', () => {
+  it('should merge empty array of Uint8Array', async () => {
+    const arrays: Uint8Array[] = [];
+    const actual: Uint8Array = mergeUint8Arrays(arrays);
+    const expect = new Uint8Array([]);
+    assert.deepStrictEqual(actual, expect);
+  });
+
+  it('should merge array of Uint8Array', async () => {
+    const arrays: Uint8Array[] = [
+      new Uint8Array([1, 2, 3]),
+      new Uint8Array([4, 5, 6, 7]),
+      new Uint8Array([8, 9]),
+    ];
+    const actual: Uint8Array = mergeUint8Arrays(arrays);
+    const expect = new Uint8Array([
+      1, 2, 3, 4, 5, 6, 7, 8, 9
+    ]);
+    assert.deepStrictEqual(actual, expect);
   });
 });
