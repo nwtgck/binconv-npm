85e3b6b3ef0c1b4e60f7d81e372fb8590dfe4cfb
Ryo Ota <nwtgck@gmail.com>
2019-09-29 10:13:57 +0900

    [test] Add a test for blobToReadableStream()

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,8 @@
 import * as assert from 'power-assert';
 
 import {mergeUint8Arrays} from '../src/mergeUint8Arrays';
+import {blobToReadableStream} from '../src/blobToReadableStream';
+
 
 describe('mergeUint8Arrays', () => {
   it('should merge empty array of Uint8Array', async () => {
@@ -21,5 +23,24 @@ describe('mergeUint8Arrays', () => {
       1, 2, 3, 4, 5, 6, 7, 8, 9
     ]);
     assert.deepStrictEqual(actual, expect);
+  });
+});
+
+describe('blobToReadableStream', () => {
+  it('should convert blob to ReadableStream', async () => {
+    const blob = new Blob(["Lorem ipsum dolor sit amet, altera quidam in pro."]);
+    const readableStream: ReadableStream<Uint8Array> = blobToReadableStream(blob);
+    const reader = readableStream.getReader();
+    const arrays: Uint8Array[] = [];
+    while (true) {
+      const {value, done} = await reader.read();
+      if (done) break;
+      arrays.push(value);
+    }
+    const array: Uint8Array = mergeUint8Arrays(arrays);
+    const expect: Uint8Array = new Uint8Array([
+      76,111,114,101,109,32,105,112,115,117,109,32,100,111,108,111,114,32,115,105,116,32,97,109,101,116,44,32,97,108,116,101,114,97,32,113,117,105,100,97,109,32,105,110,32,112,114,111,46
+    ]);
+    assert.deepStrictEqual(array, expect);
   });
 });
